3951963e09cdbd18580f55736918dfdd2a47249b
petrero <petrero@gmail.com>
2017-06-14 21:38:02 +0300

    Copy code from branch 8

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -7,14 +7,51 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Genus;
 
 class GenusController extends Controller {
   /**
-   * @Route("/genus/{genusName}")
+   * @Route("/genus/new")
+   */
+  public function newAction(){
+    $genus = new Genus();
+    $genus->setName('Octopus'.rand(1, 100));
+    $genus->setSubFamily('Octopodinae');
+    $genus->setSpeciesCount(rand(100, 99999));
+    
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($genus);
+    $em->flush();
+    
+    return new Response('<html><body>Genus Created!</body></html>');
+  }
+  
+  /**
+   * @Route("/genus")
+   */
+  public function listAction(){
+    $em = $this->getDoctrine()->getManager();
+    $genuses = $em->getRepository('AppBundle:Genus')
+      ->findAll();
+    
+    return $this->render('genus/list.html.twig', [
+      'genuses' => $genuses
+    ]);
+  }
+
+  /**
+   * @Route("/genus/{genusName}", name="genus_show")
    */
   public function showAction($genusName) {
-    $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+    $em = $this->getDoctrine()->getManager();
+    $genus = $em->getRepository('AppBundle:Genus')
+      ->findOneBy(['name' => $genusName]);
+    
+    if(!$genus){
+      throw $this->createNotFoundException('No genus found');
+    }
 
+    /*
     $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
     $key = md5($funFact);
     if ($cache->contains($key)) {
@@ -25,13 +62,13 @@ class GenusController extends Controller {
           ->transform($funFact);
       $cache->save($key, $funFact);
     }
-
+    */
+    
     $this->get('logger')
       ->info('Showing genus: '.$genusName);
 
     return $this->render('genus/show.html.twig', array(
-      'name' => $genusName,
-      'funFact' => $funFact,
+      'genus' => $genus
     ));
   }
 
