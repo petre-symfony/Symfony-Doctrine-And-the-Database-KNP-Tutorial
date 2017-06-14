1c0c3a0554e5945909f08b24b4594a0b40d9a948
petrero <petrero@gmail.com>
2017-06-14 18:43:05 +0300

    Next, head to the bottom of the class to create the getter and setter methods. But wait! Realize: you might not need getters and setters for every field, so only add them if you need them. We will need them, so open 'Code'->'Generate' again and select 'Getters and Setters'. And that's it! Create the properties, generate the annotations and generate the getters and setters if you need them.

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -42,4 +42,28 @@ class Genus {
   public function setName($name){
     $this->name = $name;
   }
+  
+  public function getSubFamily(){
+    return $this->subFamily;
+  }
+  
+  public function setSubFamily($subFamily){
+    $this->subFamily = $subFamily;
+  }
+  
+  public function getSpeciesCount(){
+    return $this->speciesCount;
+  }
+  
+  public function setSpeciesCount($speciesCount){
+    $this->speciesCount = $speciesCount;
+  }
+  
+  public function getFunFact(){
+    return $this->funFact;
+  }
+  
+  public function setFunFact($funFact){
+    $this->funFact = $funFact;
+  }
 }
