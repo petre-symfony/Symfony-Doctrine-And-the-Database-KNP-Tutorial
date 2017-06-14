0bb40a83e55d36739ba49af9daf5bdf99800a086
petrero <petrero@gmail.com>
2017-06-14 18:50:00 +0300

    Copy code from branch 4

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,69 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity;
+ * @ORM\Table(name="genus")
+ */
+class Genus {
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   * @ORM\Column(type="integer")
+   */
+  private $id;
+  
+  /**
+   * @ORM\Column(type="string")
+   */
+  private $name;
+  
+  /**
+   * @ORM\Column(type="string")
+   */
+  private $subFamily;
+  
+  /**
+   * @ORM\Column(type="integer")
+   */
+  private $speciesCount;
+  
+  /**
+   * @ORM\Column(type="string")
+   */
+  private $funFact;
+  
+  public function getName(){
+    return $this->name;
+  }
+  
+  public function setName($name){
+    $this->name = $name;
+  }
+  
+  public function getSubFamily(){
+    return $this->subFamily;
+  }
+  
+  public function setSubFamily($subFamily){
+    $this->subFamily = $subFamily;
+  }
+  
+  public function getSpeciesCount(){
+    return $this->speciesCount;
+  }
+  
+  public function setSpeciesCount($speciesCount){
+    $this->speciesCount = $speciesCount;
+  }
+  
+  public function getFunFact(){
+    return $this->funFact;
+  }
+  
+  public function setFunFact($funFact){
+    $this->funFact = $funFact;
+  }
+}
